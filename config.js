88fe21afd4e3d5db097e12b172dfa1436886010c
Nikky Hwang <nhwang@eagles.ewu.edu>
2019-05-28 14:07:46 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/278bc99d-5d15-4c3a-8729-9a8c0be32387/token ";
+const instanceLocator = "v1:us1:278bc99d-5d15-4c3a-8729-9a8c0be32387";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
+
